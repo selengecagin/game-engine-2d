e302ec159615245a779051253e88f18df0fd311e
selengecagin <62332439+selengecagin@users.noreply.github.com>
2024-07-31 09:17:16 +0300

    Implement Singleton pattern for Window class

--- src/main/java/jade/Window.java
+++ src/main/java/jade/Window.java
@@ -1,8 +1,24 @@
 package jade;
 
 public class Window {
+private int width, height;
+private String title;
+
+private static Window window = null;
+
 
     //private constructor
-    private Window(){}
+    private Window(){
+        this.width = 1920;
+        this.height = 1080;
+        this.title = "Jade Engine";
+    }
+
+public static Window get() {
+    if (Window.window == null) {
+        Window.window = new Window();
+    }
+    return Window.window;
+}
 
 }
